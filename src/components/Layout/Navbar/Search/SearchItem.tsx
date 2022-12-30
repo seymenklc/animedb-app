500232f952d2470210c7d2aa2f90e59a8eb35787
seymennklc <seymennklc@gmail.com>
2022-12-31 01:08:43 +0300

    chore: fix search func

--- src/components/Layout/Navbar/Search/SearchItem.tsx
+++ src/components/Layout/Navbar/Search/SearchItem.tsx
@@ -11,17 +11,18 @@ export default function SearchItem({ item, handleSearchItemClick }: Props) {
       <Link
          onClick={handleSearchItemClick}
          to={`/anime/${item.mal_id}`}
-         className="flex bg-slate-800 p-3 rounded items-center"
+         className="flex bg-slate-800 p-3 rounded items-center
+          hover:bg-slate-900 transition duration-300 ease-in-out"
       >
          <img
             className="w-20 h-28 rounded-md object-cover"
             src={item.images.webp.large_image_url}
             alt={item.title}
          />
-         <div className="flex flex-col gap-3 ml-4">
+         <div className="flex flex-col gap-3 ml-4 cursor-pointer">
             <h1 className="title text-base font-semibold text-slate-200">{item.title}</h1>
             <p className="text-sm text-slate-400">
-               {item?.synopsis?.slice(0, 100)}
+               {item?.synopsis?.slice(0, 100)}...
             </p>
          </div>
       </Link>
