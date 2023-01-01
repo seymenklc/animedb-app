742ec29791a8889db5681533d54bf96672fd22c8
seymennklc <seymennklc@gmail.com>
2023-01-01 18:25:39 +0300

    fix details page statics

--- src/pages/Details/Info/Statics.tsx
+++ src/pages/Details/Info/Statics.tsx
@@ -1,34 +1,37 @@
 import { Fragment } from "react";
 import { SingleAnime } from "@/types";
+import { capitalizeFirstLetter } from "@/utils";
 
 interface Props {
    anime?: SingleAnime;
+}
+
+const keys = ['type', 'score', 'rank', 'popularity', 'episodes', 'year', 'season', 'status', 'studios'];
+
+function isAnAnimeProperty(str: string): str is keyof SingleAnime {
+   return keys.includes(str);
 }
 
 export default function Statics({ anime }: Props) {
    return (
       <Fragment>
          <div className="flex flex-col gap-1 title text-lg">
-            <span>Type</span>
-            <span>Score:</span>
-            <span>Ranked:</span>
-            <span>Populerity:</span>
-            <span>Episodes:</span>
-            {anime?.episodes && <span>Year:</span>}
-            <span>Season:</span>
-            <span>Status:</span>
-            <span>Studios:</span>
+            {keys.map(key => (
+               <span key={key}>
+                  {capitalizeFirstLetter(key)}:
+               </span>
+            ))}
          </div>
          <div className="flex flex-col gap-1 text-end">
-            <span>{anime?.type}</span>
-            <span>{anime?.score}</span>
-            <span>{anime?.rank}</span>
-            <span>{anime?.popularity}</span>
-            <span>{anime?.episodes}</span>
-            <span>{anime?.year}</span>
-            <span>{anime?.season}</span>
-            <span>{anime?.status}</span>
-            <span>{anime?.studios[0].name}</span>
+            {keys.map((key: string) => {
+               if (anime && isAnAnimeProperty(key) && anime[key] !== null) {
+                  if (key === 'studios' && anime.studios.length > 0) {
+                     return <span key={key}>{anime[key][0].name}</span>;
+                  }
+                  return <span key={key}>{anime[key]}</span>;
+               }
+               return <span key={key}>-</span>;
+            })}
          </div>
       </Fragment>
    );
