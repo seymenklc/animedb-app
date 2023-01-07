c9fd1a2e301413e88e824638806f538122ba8630
seymennklc <seymennklc@gmail.com>
2023-01-07 23:45:11 +0300

    chore: fix filtering bugs

--- src/pages/Search/index.tsx
+++ src/pages/Search/index.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 // redux
 import { useGetAnimeByGenreQuery } from "@/features/anime/animeApiSlice";
@@ -10,16 +11,23 @@ import Pagination from "@/components/Elements/Pagination";
 import { usePagination } from "@/hooks/usePagination";
 
 export default function Search() {
+   const [currentGenre, setCurrentGenre] = useState<string>('All');
+
    const { id } = useParams();
    const { data, isFetching } = useGetAnimeByGenreQuery(id as string, {
       refetchOnMountOrArgChange: true
    });
    const { currentItems, pageNums, paginate } = usePagination(data?.data, 22);
 
+   useEffect(() => {
+      paginate(1);
+   }, [currentGenre]);
+
    return (
-      <div className="flex flex-col w-full gap-8">
-         <TopBar />
+      <div className="flex flex-col w-full gap-4">
+         <TopBar setCurrentGenre={setCurrentGenre} />
          <section className="flex flex-col items-start ml-6">
+            <h4 className="title text-center w-full">{currentGenre}</h4>
             <Pagination pageNums={pageNums} paginate={paginate} />
             <div className="flex flex-wrap justify-center items-center gap-5 my-5">
                {isFetching && <Skeleton count={22} width={192} height={288} />}
